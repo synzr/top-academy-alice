926fe6891cce67d026db269217a636f7e0d9e3ce
synzr <mikhail@synzr.space>
2024-01-16 19:53:57 +0500

    feat(server): hello world

--- server.ts
+++ server.ts
@@ -1,1 +1,8 @@
-console.log("Hello via Bun!");
+function fetch (request: Request): Response {
+  return Response.json({
+    response: { text: 'Привет мир!', tts: 'Привет мир!', end_session: true },
+    version: '1.0'
+  });
+}
+
+Bun.serve({ fetch: fetch });
